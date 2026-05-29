04ee987ec9ab99b0614c50cecacb908c23363567
Keerthi <rhkeerthi3@gmail.com>
2026-05-29 22:35:55 +0530

    completed Product entity

--- src/main/java/com/Ecom/E_commerce/models/Product.java
+++ src/main/java/com/Ecom/E_commerce/models/Product.java
@@ -1,4 +1,36 @@
 package com.Ecom.E_commerce.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "Product")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Product {
+
+    @Id
+    private String pro_id;
+
+    @Column(nullable = false)
+    private  String name;
+
+    @Column(nullable = false)
+    private  double price;
+
+    @Column(nullable = false)
+    private String stock;
+
+    @Column(nullable = false)
+    private String description;
+
 }
--- src/main/java/com/Ecom/E_commerce/models/User.java
+++ src/main/java/com/Ecom/E_commerce/models/User.java
@@ -32,4 +32,6 @@ public class User {
 
     @Column(nullable = true)
     private  String role;
+
+
 }
