ef4ad2372d248596f3130c0a5c8e0869dc36e7f6
Keerthi <rhkeerthi3@gmail.com>
2026-05-30 13:23:40 +0530

    completed userrepository

--- src/main/java/com/Ecom/E_commerce/repositories/UserRepository.java
+++ src/main/java/com/Ecom/E_commerce/repositories/UserRepository.java
@@ -1,4 +1,13 @@
 package com.Ecom.E_commerce.repositories;
 
-public class UserRepository {
+
+import com.Ecom.E_commerce.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepository  extends  JpaRepository<User, String>{
+
+
 }
