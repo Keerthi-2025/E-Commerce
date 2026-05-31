a6e04844ed1cc3effdd3cf4ddc06a6aa2374c917
Keerthi <rhkeerthi3@gmail.com>
2026-05-31 20:50:26 +0530

    modified files

--- src/main/java/com/Ecom/E_commerce/services/user_service/UserServiceImpl.java
+++ src/main/java/com/Ecom/E_commerce/services/user_service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.Ecom.E_commerce.services.user_service;
 
+import com.Ecom.E_commerce.mappers.UserMapper;
 import com.Ecom.E_commerce.models.User;
+
 import com.Ecom.E_commerce.repositories.UserRepository;
 import org.springframework.security.config.core.userdetails.UserDetailsMapFactoryBean;
 import org.springframework.stereotype.Service;
@@ -9,22 +11,33 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private  final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+
+
+    private  UserServiceImpl(UserRepository userRepository, UserMapper userMapper){
+
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
 
 
     @Override
     public String createUser(String userId, String userName, String password, String email) {
-
-        return "";
+        User user = userMapper.touser(userId,userName,password,email);
+        userRepository.save(user);
+        return "User created";
     }
 
     @Override
     public List<User> getAllUsers() {
-        return List.of();
+        return userRepository.findAll();
     }
 
     @Override
     public User getUserId(String userId) {
-        return null;
+        return userRepository.findById(userId).orElseThrow(()-> new ApiRequestException("User ID not found"));
     }
 }
 
