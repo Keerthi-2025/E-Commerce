1c0e4771e95103f0a94a04a25a6a2f40152a1608
Keerthi <rhkeerthi3@gmail.com>
2026-06-02 16:07:12 +0530

    completed productmapper

---
+++ src/main/java/com/Ecom/E_commerce/mappers/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.Ecom.E_commerce.mappers;
+
+
+import com.Ecom.E_commerce.models.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    public Product toproduct(String pro_id, String name, double price, String stock, String description){
+        return Product.builder()
+                .pro_id(pro_id)
+                .name(name)
+                .price(price)
+                .description(description)
+                .build();
+    }
+}
