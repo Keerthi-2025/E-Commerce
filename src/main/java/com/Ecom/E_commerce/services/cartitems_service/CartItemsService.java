6aaf20fbb0d3d31ada1ae468919616cf6a6b1a86
Keerthi <rhkeerthi3@gmail.com>
2026-06-03 17:10:22 +0530

    completed cartitemservice

--- src/main/java/com/Ecom/E_commerce/services/cartitems_service/CartItemsService.java
+++ src/main/java/com/Ecom/E_commerce/services/cartitems_service/CartItemsService.java
@@ -1,4 +1,14 @@
 package com.Ecom.E_commerce.services.cartitems_service;
 
+import com.Ecom.E_commerce.models.CartItems;
+
+import java.util.List;
+
 public interface CartItemsService {
+
+    String addItemToCart(Integer cartItm_id, String car_qty,Integer cart_id, Integer pro_id);
+
+    CartItems getCartItemsById(Integer cartItm_id);
+
+    List<CartItems> getAllCartItems();
 }
