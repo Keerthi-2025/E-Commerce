923ea0b6f55af8c7b1e9e9a8b60b2c373ac62527
Keerthi <rhkeerthi3@gmail.com>
2026-06-03 14:44:05 +0530

    completed cartMapper

--- src/main/java/com/Ecom/E_commerce/mappers/CartMapper.java
+++ src/main/java/com/Ecom/E_commerce/mappers/CartMapper.java
@@ -1,4 +1,14 @@
 package com.Ecom.E_commerce.mappers;
 
+
+import com.Ecom.E_commerce.models.Cart;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CartMapper {
+    public Cart tocart(Integer cart_id){
+        return Cart.builder()
+                .cart_id(cart_id)
+                .build();
+    }
 }
