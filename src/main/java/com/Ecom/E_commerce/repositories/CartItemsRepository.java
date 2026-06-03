3687dc2e18cf1450fb1bc3b58ce02c43493aefa3
Keerthi <rhkeerthi3@gmail.com>
2026-06-03 20:14:00 +0530

    completed CartItensRepository

---
+++ src/main/java/com/Ecom/E_commerce/repositories/CartItemsRepository.java
@@ -0,0 +1,8 @@
+package com.Ecom.E_commerce.repositories;
+
+import com.Ecom.E_commerce.models.CartItems;
+import com.Ecom.E_commerce.services.cartitems_service.CartItemsService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemsRepository extends JpaRepository<String, CartItems> {
+}
--- src/main/java/com/Ecom/E_commerce/services/cartitems_service/CartItemsServiceImpl.java
+++ src/main/java/com/Ecom/E_commerce/services/cartitems_service/CartItemsServiceImpl.java
@@ -1,4 +1,9 @@
 package com.Ecom.E_commerce.services.cartitems_service;
 
-public class CartItemsServiceImpl {
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartItemsServiceImpl implements  CartItemsService {
+    private final CartItemsRepository
 }
