a23394e55bfd247728039cabc34967959a2275be
Keerthi <rhkeerthi3@gmail.com>
2026-06-04 14:49:27 +0530

    completed orderservice

---
+++ src/main/java/com/Ecom/E_commerce/services/orders_service/OrdersService.java
@@ -0,0 +1,18 @@
+package com.Ecom.E_commerce.services.orders_service;
+
+import com.Ecom.E_commerce.models.Orders;
+import com.Ecom.E_commerce.models.User;
+
+import java.util.List;
+
+public interface OrdersService {
+
+    String createOrders( Integer order_id, String total_amt, String user_id, Integer orderItems);
+
+    Orders getOrdersById(Integer order_id);
+
+    List<Orders> getAllOrders();
+
+    List<Orders> findByUser(User user);
+
+}
---
+++ src/main/java/com/Ecom/E_commerce/services/orders_service/OrdersServiceImpl.java
@@ -0,0 +1,4 @@
+package com.Ecom.E_commerce.services.orders_service;
+
+public class OrdersServiceImpl {
+}
