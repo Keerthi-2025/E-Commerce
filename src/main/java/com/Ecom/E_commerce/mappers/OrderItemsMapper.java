c7045d88bcb9d167aab34de3c60d74a189a49958
Keerthi <rhkeerthi3@gmail.com>
2026-06-06 23:19:10 +0530

    completed orderitemsmapper

--- src/main/java/com/Ecom/E_commerce/mappers/OrderItemsMapper.java
+++ src/main/java/com/Ecom/E_commerce/mappers/OrderItemsMapper.java
@@ -1,4 +1,23 @@
 package com.Ecom.E_commerce.mappers;
 
+import com.Ecom.E_commerce.models.OrderItems;
+import com.Ecom.E_commerce.models.Orders;
+import com.Ecom.E_commerce.models.Product;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderItemsMapper {
+
+    public OrderItems tooorderitems(Integer orderItms_Id,
+                                    Integer ord_qty,
+                                    Orders orders,
+                                    Product product) {
+
+        return OrderItems.builder()
+                .orderItms_Id(orderItms_Id)
+                .ord_qty(ord_qty)
+                .orders(orders)
+                .product(product)
+                .build();
+    }
 }
